05c5bfdbda945e8c78ce9c4f9a03e783759463b5
Philip Poppler <philpoppler@gmail.com>
2019-07-13 17:28:17 -0700

    Add Modules to Users Schema
    Add current modules and lessons per module that user has completed/not started/currently undergoing

--- src/modules/providers/UserAuthentication.js
+++ src/modules/providers/UserAuthentication.js
@@ -3,13 +3,18 @@ import { gql } from "apollo-boost";
 import { Mutation } from "react-apollo";
 
 export const LOGIN_USER = gql`
-  mutation($email: String!, $password: String!){
+  mutation($email: String!, $password: String!) {
     loginUser(input: { email: $email, password: $password }) {
       token
-      user{
+      user {
         name
         email
-        
+        current_modules {
+          name
+          current_lessons
+          completed_lessons
+          incomplete_lessons
+        }
       }
     }
   }
--- src/pages/courseDashboard/CourseDashboard.js
+++ src/pages/courseDashboard/CourseDashboard.js
@@ -5,66 +5,100 @@ import Navigation from "../../Components/Navigation";
 import happyPlanet from "../../images/Happy Planet.png";
 import sadPlanet from "../../images/Sad Planet.png";
 import neutralPlanet from "../../images/Neutral Planet.png";
+import { AUTH_TOKEN, USER_INFO } from "../../constants";
 import "../../css/CourseDashboard.css";
 
-export default class CourseDashboard extends Component{
-    render() {
-        return(
-            <div id = "background"><br /><br />
-            
-            <Container className="courseDashboard">
-                <Row>
-                    <Col>
-                    <p>Variables</p>
-                    <img src={happyPlanet} className="planet" alt="module"/>
-                    </Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col>
-                    <p>Keywords</p>
-                    <a href="../questions"><img src={neutralPlanet} className="planet" alt="module"/></a>
-                    </Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col>
-                    <p>Strings</p>
-                    <img src={sadPlanet} className="planet" alt="module"/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={3}></Col>
-                    <Col md={3}></Col>
-                    <Col md={3}></Col>
-                    <Col md={3}></Col>
-                    <Col md={3}></Col>
-                    <Col md={3}></Col>
-                    <Col md={3}></Col>
-                </Row>
-                <Row>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col>
-                    <p>Expressions</p>
-                    <img src={happyPlanet} className="planet" alt="module"/>
-                    </Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col>
-                    <p>Conditionals</p>
-                    <img src={sadPlanet} className="planet" alt="module"/>
-                    </Col>
-                    <Col></Col>
-                </Row><br /><br /><br />
-                <center>
-                    <ProgressBar style={{ maxWidth: 1000, minHeight: 25 }}>
-                        <ProgressBar striped variant="success" animated now={40} key={1}/>
-                        <ProgressBar striped variant="warning" animated now={20} key={2}/>
-                    </ProgressBar ><br />
-                    <h1>Python</h1>
-                </center>
-            </Container>
-            </div>
-            
-        )
-    }
+export default class CourseDashboard extends Component {
+  state = {
+    user: "",
+    module: {}
+  };
+
+  async componentDidMount() {
+    await this.setState({ user: JSON.parse(localStorage.getItem(USER_INFO)) });
+    await this.setState({ module: this.state.user.current_modules[0] });
+    
+  }
+
+  render() {
+    return (
+      <div id="background">
+        <br />
+        <br />
+
+        <div>{this.state.user.name}</div>
+        <div>{this.state.module.name}</div>
+
+        <Container className="courseDashboard">
+          <Row>
+            <Col>
+              <p>Variables</p>
+              <img src={happyPlanet} className="planet" alt="module" />
+            </Col>
+            <Col />
+            <Col />
+            <Col>
+              <p>Keywords</p>
+              <a href="../questions">
+                <img src={neutralPlanet} className="planet" alt="module" />
+              </a>
+            </Col>
+            <Col />
+            <Col />
+            <Col>
+              <p>Strings</p>
+              <img src={sadPlanet} className="planet" alt="module" />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={3} />
+            <Col md={3} />
+            <Col md={3} />
+            <Col md={3} />
+            <Col md={3} />
+            <Col md={3} />
+            <Col md={3} />
+          </Row>
+          <Row>
+            <Col />
+            <Col />
+            <Col>
+              <p>Expressions</p>
+              <img src={happyPlanet} className="planet" alt="module" />
+            </Col>
+            <Col />
+            <Col />
+            <Col>
+              <p>Conditionals</p>
+              <img src={sadPlanet} className="planet" alt="module" />
+            </Col>
+            <Col />
+          </Row>
+          <br />
+          <br />
+          <br />
+          <center>
+            <ProgressBar style={{ maxWidth: 1000, minHeight: 25 }}>
+              <ProgressBar
+                striped
+                variant="success"
+                animated
+                now={40}
+                key={1}
+              />
+              <ProgressBar
+                striped
+                variant="warning"
+                animated
+                now={20}
+                key={2}
+              />
+            </ProgressBar>
+            <br />
+            <h1>Python</h1>
+          </center>
+        </Container>
+      </div>
+    );
+  }
 }
